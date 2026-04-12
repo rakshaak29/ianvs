e2c8393ad7660663eb10accf6f723415de58573d
rakshaak29 <rakshak29@gmail.com>
2026-04-12 23:07:30 +0530

    fix(llm_simple_qa): safe check for torch.backends.mps to support older torch versions
    Signed-off-by: rakshaak29 <rakshak29@gmail.com>

--- examples/llm_simple_qa/testalgorithms/gen/basemodel.py
+++ examples/llm_simple_qa/testalgorithms/gen/basemodel.py
@@ -27,7 +27,7 @@ from sedna.common.class_factory import ClassType, ClassFactory
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "mps" if (hasattr(torch.backends, "mps") and torch.backends.mps.is_available()) else "cpu"
 
 logging.disable(logging.WARNING)
 
